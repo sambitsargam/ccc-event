4cacc78f0b223497aaa64989c2c2572240991443
like cat <50100966+810505339@users.noreply.github.com>
2022-06-27 14:32:54 +0800

    Update Game.vue
    v-model

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -32,8 +32,8 @@
      
       <h2>Roll Number 
         <input type="text" name="Roll Number" /></h2>
- <input type="hidden" name="timer" id="time" value="" /> 
- <input type="hidden" name="moves" id="move" value="" />
+ <input type="hidden" name="timer" id="time" v-model="timer"  value="" /> 
+ <input type="hidden" name="moves" id="move" v-model="moves" value="" />
       <button type="submit" :disabled="submitting" class="new-game">Submit Details</button>
       </form>
    
